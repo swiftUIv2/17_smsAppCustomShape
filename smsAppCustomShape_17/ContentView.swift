ae7f1d1a1f2d33330876b7fbf60e42436b98efd7
emm <723125@gmail.com>
2021-02-02 14:57:16 +0000

    Initial Commit

---
+++ smsAppCustomShape_17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  smsAppCustomShape_17
+//
+//  Created by emm on 02/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ smsAppCustomShape_17/smsAppCustomShape_17App.swift
@@ -0,0 +1,17 @@
+//
+//  smsAppCustomShape_17App.swift
+//  smsAppCustomShape_17
+//
+//  Created by emm on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct smsAppCustomShape_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
